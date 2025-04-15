b35e07f1bd521079dc675feea5685798180910e9
Blasonic <168122520+Blasonic@users.noreply.github.com>
2025-04-15 11:34:18 +0200

    perfilll

--- src/back/modelo/Usuario.js
+++ src/back/modelo/Usuario.js
@@ -5,7 +5,8 @@ const UsuarioSchema = new mongoose.Schema({
   user: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  profilePicture: { type: String, default: '' } 
+  profilePicture: { type: String, default: '' }
 });
+
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
